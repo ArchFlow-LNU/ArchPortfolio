7f313ebeeecc2eb582b51802b3f4360730455a95
brbthrt <sav.roxolana@email.com>
2026-03-18 17:29:09 +0200

    files with packages and configrations

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using ArchPortfolio.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder  = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-
+    
 var app = builder.Build();
 
 app.UseRouting();
