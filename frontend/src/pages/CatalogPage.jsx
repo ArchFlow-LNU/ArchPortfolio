5303287caaf59c8055e5d85d12bdd2fba3692e10
Твоє Ім'я <@email.com>
2026-03-19 07:23:56 +0200

    update catalog page

--- frontend/src/pages/CatalogPage.jsx
+++ frontend/src/pages/CatalogPage.jsx
@@ -1,4 +1,5 @@
 import HouseVariants from "../components/HouseVariants.jsx";
+import BestProjects from "../components/BestProjects.jsx";
 import NavBar from "../components/NavBar.jsx";
 
 export default function CatalogPage(){
