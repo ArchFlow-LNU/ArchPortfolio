427b6d04f608eccda9f89fe37588a0a871567b09
brbthrt <sav.roxolana@email.com>
2026-03-25 14:37:09 +0200

    axios.get for projects in MainPage

--- frontend/src/components/HouseVariants.jsx
+++ frontend/src/components/HouseVariants.jsx
@@ -1,26 +1,35 @@
 import "../css/HouseVariants.css"
+import {useState, useEffect} from "react";
+import axios from "axios"
 export default function HouseVariants() {
 
-    const houses = [
-        {
-            title: "Мінімалістичний будинок",
-            img: "/imgs/house1.png",
-            desc: "Сучасний будинок із простими формами, панорамними вікнами та відкритим простором.",
-            style: "Мінімалізм"
-        },
-        {
-            title: "Міська резиденція",
-            img: "/imgs/house2.png",
-            desc: "Сучасний житловий комплекс для комфортного життя в місті.",
-            style: "Сучасний"
-        },
-        {
-            title: "Клубний будинок",
-            img: "/imgs/house3.png",
-            desc: "Елегантний будинок із класичними елементами та сучасним плануванням.",
-            style: "Сучасний / Преміум"
-        }
-    ]
+    // const houses = [
+    //     {
+    //         title: "Мінімалістичний будинок",
+    //         img: "/imgs/house1.png",
+    //         desc: "Сучасний будинок із простими формами, панорамними вікнами та відкритим простором.",
+    //         style: "Мінімалізм"
+    //     },
+    //     {
+    //         title: "Міська резиденція",
+    //         img: "/imgs/house2.png",
+    //         desc: "Сучасний житловий комплекс для комфортного життя в місті.",
+    //         style: "Сучасний"
+    //     },
+    //     {
+    //         title: "Клубний будинок",
+    //         img: "/imgs/house3.png",
+    //         desc: "Елегантний будинок із класичними елементами та сучасним плануванням.",
+    //         style: "Сучасний / Преміум"
+    //     }
+    // ]
+
+    const [projects,setProjects]=useState([])
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/projects")
+            .then(res => setProjects(res.data))
+            .catch(err => console.log("error fetch",err));
+    }, []);
 
     return (
         <section className="houses">
@@ -29,15 +38,15 @@ export default function HouseVariants() {
 
             <div className="house-grid">
 
-                {houses.map((h, i) => (
+                {projects.map((h, i) => (
                     <div className="house-card" key={i}>
 
-                        <img src={h.img} alt={h.title} />
+                        <img src="/imgs/house1.png" alt={h.title} />
 
                         <div className="house-info">
                             <h3>{h.title}</h3>
-                            <p>{h.desc}</p>
-                            <span>Стиль: {h.style}</span>
+                            <p>{h.description}</p>
+                            <span>Стиль: dkdkd</span>
                         </div>
 
                     </div>
