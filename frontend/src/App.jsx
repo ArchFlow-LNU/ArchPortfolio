5ec90e3bb9e18e3e79496fecfe60dc09fe0e7fe5
Твоє Ім'я <@email.com>
2026-04-02 15:03:08 +0300

    fix: protect admin routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,57 @@
-import {Route,Routes} from 'react-router-dom'
-import "./App.css"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
 import MainPage from "./pages/MainPage.jsx";
 import CatalogPage from "./pages/CatalogPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
+import HousePage from "./pages/HousePage.jsx";
+import ReviewsPage from "./pages/ReviewPage.jsx";
+
 import LoginPage from "./admin/adminPages/LoginPage.jsx";
 import RegisterPage from "./admin/adminPages/RegisterPage.jsx";
 import ProfilePage from "./admin/adminPages/ProfilePage.jsx";
 import ProjectFormPage from "./admin/adminPages/ProjectFormPage.jsx";
-import HousePage from "./pages/HousePage.jsx";
-import ReviewsPage from "./pages/ReviewPage.jsx";
+
+import ProtectedRoute from "./admin/adminComponents/ProtectedRoute.jsx";
 
 function App() {
     return (
         <Routes>
-                <Route path="/" element={<MainPage></MainPage>}></Route>
-                <Route path='/catalog' element={<CatalogPage></CatalogPage>}></Route>
-                <Route path='/about' element={<AboutPage></AboutPage>}></Route>
-                <Route path='/contacts' element={<ContactPage></ContactPage>}></Route>
-                <Route path="/admin/login" element={<LoginPage></LoginPage>}></Route>
-                <Route path="/admin/register" element={<RegisterPage></RegisterPage>}></Route>
-                <Route path="/admin/profile" element={<ProfilePage></ProfilePage>}></Route>
-                <Route path="/admin/profile/new" element={<ProjectFormPage></ProjectFormPage>}></Route>
-                <Route path="/house/:id" element={<HousePage />} />
-                <Route path="/reviews" element={<ReviewsPage />} />
-        </Routes>
 
-    )
+            {/* PUBLIC */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contacts" element={<ContactPage />} />
+            <Route path="/house/:id" element={<HousePage />} />
+            <Route path="/reviews" element={<ReviewsPage />} />
+
+            {/* AUTH */}
+            <Route path="/admin/login" element={<LoginPage />} />
+            <Route path="/admin/register" element={<RegisterPage />} />
+
+            {/* PROTECTED */}
+            <Route
+                path="/admin/profile"
+                element={
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/admin/profile/new"
+                element={
+                    <ProtectedRoute>
+                        <ProjectFormPage />
+                    </ProtectedRoute>
+                }
+            />
+
+        </Routes>
+    );
 }
 
-export default App
+export default App;
