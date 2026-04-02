9f27e5d8cedc5e0fdf01e79ae6ad4e5f35e3a290
Твоє Ім'я <@email.com>
2026-04-02 14:55:13 +0300

    add login and register logic with axios

--- frontend/src/admin/adminPages/RegisterPage.jsx
+++ frontend/src/admin/adminPages/RegisterPage.jsx
@@ -1,17 +1,53 @@
 import "../adminCss/RegisterPage.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../api/axios";
+
 export default function RegisterPage() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            await api.post("/api/auth/register", {
+                email,
+                password
+            });
+
+            navigate("/admin/login");
+
+        } catch {
+            alert("Користувач вже існує");
+        }
+    };
+
     return (
         <div className="register-page">
             <div className="wrapper2">
                 <h1 className="h1">Register</h1>
 
-                <form action="" className="register-form">
-                    <input type="text" placeholder="enter your name"/>
-                    <input type="email" placeholder="input your emain"/>
-                    <input type="password" placeholder="input your password"/>
-                </form>
-                <button className="register-btn">Register</button>
+                <form className="register-form" onSubmit={handleRegister}>
+                    <input
+                        type="email"
+                        placeholder="input your email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
+                    <input
+                        type="password"
+                        placeholder="input your password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className="register-btn">Register</button>
+                </form>
             </div>
         </div>
     )
