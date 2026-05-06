105b57997f8da3dc454bfeec696425308628f369
brbthrt <sav.roxolana@email.com>
2026-05-06 21:43:22 +0300

    RM MGR

--- backend/Program.cs
+++ backend/Program.cs
@@ -58,7 +58,7 @@ using (var scope = app.Services.CreateScope())
     {
         var services = scope.ServiceProvider;
         var context = services.GetRequiredService<AppDbContext>();
-        context.Database.Migrate();
+        //context.Database.Migrate();
         Console.WriteLine("Migration successful!");
     }
     catch (Exception ex)
